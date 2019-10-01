30902af588638697adb86610022128711fb44d11
Saulius Paškevičius <sauliusp34@gmail.com>
2019-10-01 21:32:34 +0300

    Pridėta studdy buddy windows form

---
+++ StudyLabsApp/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace StudyLabsApp
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainMenuPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.StuddyBuddy = new System.Windows.Forms.Label();
+            this.EnterForums = new System.Windows.Forms.Button();
+            this.FindStuddyBuddies = new System.Windows.Forms.Button();
+            this.PanelForUniversityList = new System.Windows.Forms.Panel();
+            this.ReturnToMainMenuButton = new System.Windows.Forms.Button();
+            this.UniversityList = new System.Windows.Forms.TableLayoutPanel();
+            this.ChemistryGeosciencesButton = new System.Windows.Forms.Button();
+            this.CommunicationButton = new System.Windows.Forms.Button();
+            this.LawButton = new System.Windows.Forms.Button();
+            this.EconomicsBusinessButton = new System.Windows.Forms.Button();
+            this.HistoryButton = new System.Windows.Forms.Button();
+            this.MathematicsInformaticsButton = new System.Windows.Forms.Button();
+            this.MedicineButton = new System.Windows.Forms.Button();
+            this.PhilologyButton = new System.Windows.Forms.Button();
+            this.PhilosophyButton = new System.Windows.Forms.Button();
+            this.PhysicsButton = new System.Windows.Forms.Button();
+            this.PoliticalScienceButton = new System.Windows.Forms.Button();
+            this.KaunasButton = new System.Windows.Forms.Button();
+            this.MainMenuPanel.SuspendLayout();
+            this.PanelForUniversityList.SuspendLayout();
+            this.UniversityList.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainMenuPanel
+            // 
+            this.MainMenuPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainMenuPanel.ColumnCount = 1;
+            this.MainMenuPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.MainMenuPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.MainMenuPanel.Controls.Add(this.EnterForums, 0, 0);
+            this.MainMenuPanel.Controls.Add(this.FindStuddyBuddies, 0, 1);
+            this.MainMenuPanel.Location = new System.Drawing.Point(12, 123);
+            this.MainMenuPanel.Name = "MainMenuPanel";
+            this.MainMenuPanel.RowCount = 2;
+            this.MainMenuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.MainMenuPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.MainMenuPanel.Size = new System.Drawing.Size(641, 460);
+            this.MainMenuPanel.TabIndex = 0;
+            // 
+            // StuddyBuddy
+            // 
+            this.StuddyBuddy.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StuddyBuddy.BackColor = System.Drawing.SystemColors.Control;
+            this.StuddyBuddy.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.StuddyBuddy.Location = new System.Drawing.Point(12, 9);
+            this.StuddyBuddy.Name = "StuddyBuddy";
+            this.StuddyBuddy.Size = new System.Drawing.Size(641, 111);
+            this.StuddyBuddy.TabIndex = 1;
+            this.StuddyBuddy.Text = "Studdy buddy";
+            this.StuddyBuddy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // EnterForums
+            // 
+            this.EnterForums.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.EnterForums.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.EnterForums.Location = new System.Drawing.Point(3, 3);
+            this.EnterForums.Name = "EnterForums";
+            this.EnterForums.Size = new System.Drawing.Size(635, 224);
+            this.EnterForums.TabIndex = 0;
+            this.EnterForums.Text = "Enter forums";
+            this.EnterForums.UseVisualStyleBackColor = true;
+            this.EnterForums.Click += new System.EventHandler(this.EnterForums_Click);
+            // 
+            // FindStuddyBuddies
+            // 
+            this.FindStuddyBuddies.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FindStuddyBuddies.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.FindStuddyBuddies.Location = new System.Drawing.Point(3, 233);
+            this.FindStuddyBuddies.Name = "FindStuddyBuddies";
+            this.FindStuddyBuddies.Size = new System.Drawing.Size(635, 224);
+            this.FindStuddyBuddies.TabIndex = 1;
+            this.FindStuddyBuddies.Text = "Find studdy buddies";
+            this.FindStuddyBuddies.UseVisualStyleBackColor = true;
+            this.FindStuddyBuddies.Click += new System.EventHandler(this.FindStuddyBuddies_Click);
+            // 
+            // PanelForUniversityList
+            // 
+            this.PanelForUniversityList.Controls.Add(this.UniversityList);
+            this.PanelForUniversityList.Controls.Add(this.ReturnToMainMenuButton);
+            this.PanelForUniversityList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelForUniversityList.Location = new System.Drawing.Point(0, 0);
+            this.PanelForUniversityList.Name = "PanelForUniversityList";
+            this.PanelForUniversityList.Size = new System.Drawing.Size(665, 595);
+            this.PanelForUniversityList.TabIndex = 2;
+            this.PanelForUniversityList.Visible = false;
+            // 
+            // ReturnToMainMenuButton
+            // 
+            this.ReturnToMainMenuButton.AutoSize = true;
+            this.ReturnToMainMenuButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ReturnToMainMenuButton.Location = new System.Drawing.Point(0, 519);
+            this.ReturnToMainMenuButton.Name = "ReturnToMainMenuButton";
+            this.ReturnToMainMenuButton.Size = new System.Drawing.Size(665, 76);
+            this.ReturnToMainMenuButton.TabIndex = 0;
+            this.ReturnToMainMenuButton.Text = "Return To Main Menu";
+            this.ReturnToMainMenuButton.UseVisualStyleBackColor = true;
+            this.ReturnToMainMenuButton.Click += new System.EventHandler(this.ReturnToMainMenuButton_Click);
+            // 
+            // UniversityList
+            // 
+            this.UniversityList.ColumnCount = 3;
+            this.UniversityList.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.UniversityList.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.UniversityList.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.UniversityList.Controls.Add(this.KaunasButton, 2, 3);
+            this.UniversityList.Controls.Add(this.PoliticalScienceButton, 1, 3);
+            this.UniversityList.Controls.Add(this.PhysicsButton, 0, 3);
+            this.UniversityList.Controls.Add(this.PhilosophyButton, 2, 2);
+            this.UniversityList.Controls.Add(this.PhilologyButton, 1, 2);
+            this.UniversityList.Controls.Add(this.MedicineButton, 0, 2);
+            this.UniversityList.Controls.Add(this.MathematicsInformaticsButton, 2, 1);
+            this.UniversityList.Controls.Add(this.HistoryButton, 1, 1);
+            this.UniversityList.Controls.Add(this.EconomicsBusinessButton, 0, 1);
+            this.UniversityList.Controls.Add(this.LawButton, 2, 0);
+            this.UniversityList.Controls.Add(this.CommunicationButton, 1, 0);
+            this.UniversityList.Controls.Add(this.ChemistryGeosciencesButton, 0, 0);
+            this.UniversityList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.UniversityList.Location = new System.Drawing.Point(0, 0);
+            this.UniversityList.Name = "UniversityList";
+            this.UniversityList.RowCount = 4;
+            this.UniversityList.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.UniversityList.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.UniversityList.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.UniversityList.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.UniversityList.Size = new System.Drawing.Size(665, 519);
+            this.UniversityList.TabIndex = 1;
+            // 
+            // ChemistryGeosciencesButton
+            // 
+            this.ChemistryGeosciencesButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ChemistryGeosciencesButton.Location = new System.Drawing.Point(3, 3);
+            this.ChemistryGeosciencesButton.Name = "ChemistryGeosciencesButton";
+            this.ChemistryGeosciencesButton.Size = new System.Drawing.Size(215, 123);
+            this.ChemistryGeosciencesButton.TabIndex = 1;
+            this.ChemistryGeosciencesButton.Text = "Faculty of Chemistry and Geosciences";
+            this.ChemistryGeosciencesButton.UseVisualStyleBackColor = true;
+            // 
+            // CommunicationButton
+            // 
+            this.CommunicationButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CommunicationButton.Location = new System.Drawing.Point(224, 3);
+            this.CommunicationButton.Name = "CommunicationButton";
+            this.CommunicationButton.Size = new System.Drawing.Size(215, 123);
+            this.CommunicationButton.TabIndex = 2;
+            this.CommunicationButton.Text = "Faculty of Communication";
+            this.CommunicationButton.UseVisualStyleBackColor = true;
+            // 
+            // LawButton
+            // 
+            this.LawButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LawButton.Location = new System.Drawing.Point(445, 3);
+            this.LawButton.Name = "LawButton";
+            this.LawButton.Size = new System.Drawing.Size(217, 123);
+            this.LawButton.TabIndex = 3;
+            this.LawButton.Text = "Faculty of Law";
+            this.LawButton.UseVisualStyleBackColor = true;
+            // 
+            // EconomicsBusinessButton
+            // 
+            this.EconomicsBusinessButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.EconomicsBusinessButton.Location = new System.Drawing.Point(3, 132);
+            this.EconomicsBusinessButton.Name = "EconomicsBusinessButton";
+            this.EconomicsBusinessButton.Size = new System.Drawing.Size(215, 123);
+            this.EconomicsBusinessButton.TabIndex = 4;
+            this.EconomicsBusinessButton.Text = "Faculty of Economics and Business Administration";
+            this.EconomicsBusinessButton.UseVisualStyleBackColor = true;
+            // 
+            // HistoryButton
+            // 
+            this.HistoryButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HistoryButton.Location = new System.Drawing.Point(224, 132);
+            this.HistoryButton.Name = "HistoryButton";
+            this.HistoryButton.Size = new System.Drawing.Size(215, 123);
+            this.HistoryButton.TabIndex = 5;
+            this.HistoryButton.Text = "Faculty of History";
+            this.HistoryButton.UseVisualStyleBackColor = true;
+            // 
+            // MathematicsInformaticsButton
+            // 
+            this.MathematicsInformaticsButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MathematicsInformaticsButton.Location = new System.Drawing.Point(445, 132);
+            this.MathematicsInformaticsButton.Name = "MathematicsInformaticsButton";
+            this.MathematicsInformaticsButton.Size = new System.Drawing.Size(217, 123);
+            this.MathematicsInformaticsButton.TabIndex = 6;
+            this.MathematicsInformaticsButton.Text = "Faculty of Mathematics and Informatics";
+            this.MathematicsInformaticsButton.UseVisualStyleBackColor = true;
+            // 
+            // MedicineButton
+            // 
+            this.MedicineButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MedicineButton.Location = new System.Drawing.Point(3, 261);
+            this.MedicineButton.Name = "MedicineButton";
+            this.MedicineButton.Size = new System.Drawing.Size(215, 123);
+            this.MedicineButton.TabIndex = 7;
+            this.MedicineButton.Text = "Faculty of Medicine";
+            this.MedicineButton.UseVisualStyleBackColor = true;
+            // 
+            // PhilologyButton
+            // 
+            this.PhilologyButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PhilologyButton.Location = new System.Drawing.Point(224, 261);
+            this.PhilologyButton.Name = "PhilologyButton";
+            this.PhilologyButton.Size = new System.Drawing.Size(215, 123);
+            this.PhilologyButton.TabIndex = 8;
+            this.PhilologyButton.Text = "Faculty of Philology";
+            this.PhilologyButton.UseVisualStyleBackColor = true;
+            // 
+            // PhilosophyButton
+            // 
+            this.PhilosophyButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PhilosophyButton.Location = new System.Drawing.Point(445, 261);
+            this.PhilosophyButton.Name = "PhilosophyButton";
+            this.PhilosophyButton.Size = new System.Drawing.Size(217, 123);
+            this.PhilosophyButton.TabIndex = 9;
+            this.PhilosophyButton.Text = "Faculty of Philosophy";
+            this.PhilosophyButton.UseVisualStyleBackColor = true;
+            // 
+            // PhysicsButton
+            // 
+            this.PhysicsButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PhysicsButton.Location = new System.Drawing.Point(3, 390);
+            this.PhysicsButton.Name = "PhysicsButton";
+            this.PhysicsButton.Size = new System.Drawing.Size(215, 126);
+            this.PhysicsButton.TabIndex = 10;
+            this.PhysicsButton.Text = "Faculty of Physics";
+            this.PhysicsButton.UseVisualStyleBackColor = true;
+            // 
+            // PoliticalScienceButton
+            // 
+            this.PoliticalScienceButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PoliticalScienceButton.Location = new System.Drawing.Point(224, 390);
+            this.PoliticalScienceButton.Name = "PoliticalScienceButton";
+            this.PoliticalScienceButton.Size = new System.Drawing.Size(215, 126);
+            this.PoliticalScienceButton.TabIndex = 11;
+            this.PoliticalScienceButton.Text = "Institute of International Relations and Political Science";
+            this.PoliticalScienceButton.UseVisualStyleBackColor = true;
+            // 
+            // KaunasButton
+            // 
+            this.KaunasButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.KaunasButton.Location = new System.Drawing.Point(445, 390);
+            this.KaunasButton.Name = "KaunasButton";
+            this.KaunasButton.Size = new System.Drawing.Size(217, 126);
+            this.KaunasButton.TabIndex = 12;
+            this.KaunasButton.Text = "Kaunas Faculty";
+            this.KaunasButton.UseVisualStyleBackColor = true;
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 595);
+            this.Controls.Add(this.PanelForUniversityList);
+            this.Controls.Add(this.StuddyBuddy);
+            this.Controls.Add(this.MainMenuPanel);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "Window";
+            this.Text = "Studdy Buddy";
+            this.MainMenuPanel.ResumeLayout(false);
+            this.PanelForUniversityList.ResumeLayout(false);
+            this.PanelForUniversityList.PerformLayout();
+            this.UniversityList.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel MainMenuPanel;
+        private System.Windows.Forms.Label StuddyBuddy;
+        private System.Windows.Forms.Button EnterForums;
+        private System.Windows.Forms.Button FindStuddyBuddies;
+        private System.Windows.Forms.Panel PanelForUniversityList;
+        private System.Windows.Forms.Button ReturnToMainMenuButton;
+        private System.Windows.Forms.TableLayoutPanel UniversityList;
+        private System.Windows.Forms.Button KaunasButton;
+        private System.Windows.Forms.Button PoliticalScienceButton;
+        private System.Windows.Forms.Button PhysicsButton;
+        private System.Windows.Forms.Button PhilosophyButton;
+        private System.Windows.Forms.Button PhilologyButton;
+        private System.Windows.Forms.Button MedicineButton;
+        private System.Windows.Forms.Button MathematicsInformaticsButton;
+        private System.Windows.Forms.Button HistoryButton;
+        private System.Windows.Forms.Button EconomicsBusinessButton;
+        private System.Windows.Forms.Button LawButton;
+        private System.Windows.Forms.Button CommunicationButton;
+        private System.Windows.Forms.Button ChemistryGeosciencesButton;
+    }
+}
+
---
+++ StudyLabsApp/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudyLabsApp
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterForums_Click(object sender, EventArgs e)
+        {
+            //MainMenuPanel.Visible = false;
+            //StuddyBuddy.Visible = false;
+        }
+
+        private void FindStuddyBuddies_Click(object sender, EventArgs e)
+        {
+            MainMenuPanel.Visible = false;
+            StuddyBuddy.Visible = false;
+            PanelForUniversityList.Visible = true;
+
+        }
+
+        private void ReturnToMainMenuButton_Click(object sender, EventArgs e)
+        {
+            PanelForUniversityList.Visible = false;
+            MainMenuPanel.Visible = true;
+            StuddyBuddy.Visible = true;
+        }
+    }
+}
