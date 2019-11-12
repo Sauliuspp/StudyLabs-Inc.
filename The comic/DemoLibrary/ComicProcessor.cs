b174ab08419fc931fc7e12fc800bb890e43d08ec
mRBruman <55790338+mRBruman@users.noreply.github.com>
2019-11-12 18:46:07 +0200

    atskiras projektas
    Atskiras projektas, kur veikia komiksas.  Negaliu kartu su kitais ikelti, nes meta konfliktus.

---
+++ The comic/DemoLibrary/ComicProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class ComicProcessor
+    {
+        public static async Task<ComicModel> LoadComic(int comicNumber = 0)
+        {
+            string url = "";
+
+            if (comicNumber > 0)
+            {
+                url = $"https://xkcd.com/{ comicNumber }/info.0.json";
+            }
+            else
+            {
+                url = "https://xkcd.com/info.0.json";
+            }
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    ComicModel comic = await response.Content.ReadAsAsync<ComicModel>();
+
+                    return comic;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
---
+++ The comic/WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int maxNumber = 0;
+        private int currentNumber = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApiHelper.InitializeClient();
+            nextImageButton.IsEnabled = false;
+        }
+
+        private async Task LoadImage(int imageNumber = 0)
+        {
+            var comic = await ComicProcessor.LoadComic(imageNumber);
+
+            if (imageNumber == 0)
+            {
+                maxNumber = comic.Num;
+            }
+
+            currentNumber = comic.Num;
+
+            var uriSource = new Uri(comic.Img, UriKind.Absolute);
+            comicImage.Source = new BitmapImage(uriSource);
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await LoadImage();
+        }
+
+        private async void previousImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentNumber > 1)
+            {
+                currentNumber -= 1;
+                nextImageButton.IsEnabled = true;
+                await LoadImage(currentNumber);
+
+                if (currentNumber == 1)
+                {
+                    previousImageButton.IsEnabled = false;
+                }
+            }
+        }
+
+        private async void nextImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentNumber < maxNumber)
+            {
+                currentNumber += 1;
+                previousImageButton.IsEnabled = true;
+                await LoadImage(currentNumber);
+
+                if (currentNumber == maxNumber)
+                {
+                    nextImageButton.IsEnabled = false;
+                }
+            }
+        }
+    }
+}
